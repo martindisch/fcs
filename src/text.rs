e0c2e0c02fd32a68a8fdb476be81c91a29aad6d3
Martin Disch <martindisch@gmail.com>
2022-06-06 12:37:03 +0200

    Implement two basic building blocks

--- src/text.rs
+++ src/text.rs
@@ -1,8 +1,47 @@
+use nom::{
+    bytes::complete::{is_not, tag},
+    IResult,
+};
+
+fn not_separator(input: &str) -> IResult<&str, &str> {
+    is_not(",")(input)
+}
+
+fn escaped_separator(input: &str) -> IResult<&str, &str> {
+    tag(",,")(input)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn not_separator_single() {
+        let input = "a";
+        assert_eq!(Ok(("", "a")), not_separator(input));
+    }
+
+    #[test]
+    fn not_separator_multiple() {
+        let input = "ab";
+        assert_eq!(Ok(("", "ab")), not_separator(input));
+    }
+
+    #[test]
+    fn not_separator_terminated() {
+        let input = "ab,";
+        assert_eq!(Ok((",", "ab")), not_separator(input));
+    }
+
+    #[test]
+    fn not_separator_followed() {
+        let input = "ab,cd";
+        assert_eq!(Ok((",cd", "ab")), not_separator(input));
+    }
+
+    #[test]
+    fn escaped_separator_single() {
+        let input = ",,";
+        assert_eq!(Ok(("", ",,")), escaped_separator(input));
     }
 }
