ae75a3e2e6cf18f15245494154ee5b8263883ca8
Martin Disch <martindisch@gmail.com>
2022-06-06 12:38:07 +0200

    Combine them into unseparated string

--- src/text.rs
+++ src/text.rs
@@ -1,7 +1,14 @@
 use nom::{
+    branch::alt,
     bytes::complete::{is_not, tag},
+    combinator::recognize,
+    multi::many1,
     IResult,
 };
+
+fn unseparated_string(input: &str) -> IResult<&str, &str> {
+    recognize(many1(alt((not_separator, escaped_separator))))(input)
+}
 
 fn not_separator(input: &str) -> IResult<&str, &str> {
     is_not(",")(input)
@@ -14,6 +21,54 @@ fn escaped_separator(input: &str) -> IResult<&str, &str> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn unseparated_string_easy() {
+        let input = "ab";
+        assert_eq!(Ok(("", "ab")), unseparated_string(input));
+    }
+
+    #[test]
+    fn unseparated_string_escaped_start() {
+        let input = ",,ab";
+        assert_eq!(Ok(("", ",,ab")), unseparated_string(input));
+    }
+
+    #[test]
+    fn unseparated_string_escaped_middle() {
+        let input = "a,,b";
+        assert_eq!(Ok(("", "a,,b")), unseparated_string(input));
+    }
+
+    #[test]
+    fn unseparated_string_escaped_end() {
+        let input = "ab,,";
+        assert_eq!(Ok(("", "ab,,")), unseparated_string(input));
+    }
+
+    #[test]
+    fn unseparated_string_escaped_only() {
+        let input = ",,";
+        assert_eq!(Ok(("", ",,")), unseparated_string(input));
+    }
+
+    #[test]
+    fn unseparated_string_escaped_multiple() {
+        let input = "a,,,,b";
+        assert_eq!(Ok(("", "a,,,,b")), unseparated_string(input));
+    }
+
+    #[test]
+    fn unseparated_string_unescaped() {
+        let input = "a,b";
+        assert_eq!(Ok((",b", "a")), unseparated_string(input));
+    }
+
+    #[test]
+    fn unseparated_string_escaped_unescaped_one() {
+        let input = "a,,,b";
+        assert_eq!(Ok((",b", "a,,")), unseparated_string(input));
+    }
 
     #[test]
     fn not_separator_single() {
